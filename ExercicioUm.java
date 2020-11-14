e23c3ac26a57ffd5abf807ab3db2a563073ad172
Izaac Baptista <izaacbaptista@gmail.com>
2020-11-13 22:20:49 -0300

    first class

---
+++ ExercicioUm.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+
+public class ExercicioUm {
+    public static void main(String[] args) {
+        String productOne = "Computer";
+        String productTwo = "Office Desk";
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+        double priceOne = 2100.0;
+        double priceTwo = 650.50;
+        double measure = 53.234567;
+
+        System.out.printf("Products:\n");
+        Locale.setDefault(Locale.US);
+        System.out.printf( productOne + ", which price is $" + priceOne + "\n");
+        System.out.printf( productTwo + ", which price is $" + priceTwo + "\n\n");
+
+        System.out.printf("Record: " + age + " years old, code " + code + " and gender: " + gender + "\n\n");
+
+        System.out.println("Measue with eight decimal places: " + measure + "\n");
+        System.out.println("Rouded (three decimal places): " + measure + "\n");
+        System.out.printf("Measue with eight decimal places: " + measure + "\n");
+    }
+}
